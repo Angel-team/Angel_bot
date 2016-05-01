18958baf8bb38c41779c8723aa4be6751ed64430
Angel-team <MohsenHosseini.Angel@gmail.com>
2016-05-01 17:02:32 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {201477335,215213823},--Sudo users
+    sudo_users = {201477335},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
